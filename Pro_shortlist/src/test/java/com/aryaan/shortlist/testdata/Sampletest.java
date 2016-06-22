12caad8f388e453c2cbdf13be8bc8d63dafc94c7
kiran <kirannaoh@gmail.com>
2016-06-22 15:26:38 +0530

    package name changed

---
+++ Pro_shortlist/src/test/java/com/aryaan/shortlist/testdata/Sampletest.java
@@ -0,0 +1,14 @@
+package com.aryaan.shortlist.testdata;
+
+import org.testng.annotations.Test;
+
+public class Sampletest 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("this is the test file");
+		
+	}
+
+}
